09b8b95cba633465a60d53cd43071f761eec962c
tatsuya.tsutusmi <tatsuya223@gmail.com>
2018-12-20 13:24:02 +0000

    likes count

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,7 @@ class UsersController < ApplicationController
   def likes
     @user =  User.find(params[:id])
     @likes = @user.likings.page(params[:page])
+    counts(@user)
   end
 
   private
